0c89a358bf1df0677f49228a6d3d04f9d863eff3
WIWI-afk-creator <wissezaara39@gmail.com>
2026-09-23 15:17:23 +0200

    Update servlet URL pattern to '/AccueuilServlet'

--- src/main/java/com/mycompany/gestionclients/AccueilServlet.java
+++ src/main/java/com/mycompany/gestionclients/AccueilServlet.java
@@ -12,7 +12,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet(name="AccueilServlet", urlPatterns={"/"})
+
+@WebServlet(name="AccueilServlet", urlPatterns={"/AccueuilServlet"})
 public class AccueilServlet extends HttpServlet {
 
     @Override
